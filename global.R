bf7b811b09fab57c2380f70c131b218306d9f97e
212628167 <rupayan.neogy@ge.com>
2017-07-07 16:06:35 -0400

    initial commit

---
+++ global.R
@@ -0,0 +1,34 @@
+library(dplyr)
+library(rjson)
+
+datasets <- fromJSON(readLines("https://data.boston.gov/api/3/action/package_search?q=geojson&rows=60"))$result$results
+titles <- lapply(datasets, function(x){
+  return (x$title)
+})
+urls <- lapply(datasets, function(x){
+  return (x$resources[[1]]$url)
+})
+data <- setNames(urls,titles)
+data <- data[order(names(data))]
+
+# allzips <- readRDS("data/superzip.rds")
+# allzips$latitude <- jitter(allzips$latitude)
+# allzips$longitude <- jitter(allzips$longitude)
+# allzips$college <- allzips$college * 100
+# allzips$zipcode <- formatC(allzips$zipcode, width=5, format="d", flag="0")
+# row.names(allzips) <- allzips$zipcode
+# 
+# cleantable <- allzips %>%
+#   select(
+#     City = city.x,
+#     State = state.x,
+#     Zipcode = zipcode,
+#     Rank = rank,
+#     Score = centile,
+#     Superzip = superzip,
+#     Population = adultpop,
+#     College = college,
+#     Income = income,
+#     Lat = latitude,
+#     Long = longitude
+#   )
