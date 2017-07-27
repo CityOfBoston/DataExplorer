f2164052cc224ca18c764a08afc9919b1251a2fe
Young <212627506@logon.ds.ge.com>
2017-07-27 17:00:57 -0400

    adding maps

--- global.R
+++ global.R
@@ -52,3 +52,16 @@ getLabel <- function(data){
   }
   return(NULL)
 }
+########################
+## potential save map
+saveas <- function(map, file){
+  class(map) <- c("saveas",class(map))
+  attr(map,"filesave")=file
+  map
+}
+
+print.saveas <- function(x, ...){
+  class(x) = class(x)[class(x)!="saveas"]
+  htmltools::save_html(x, file=attr(x,"filesave"))
+}
+#######################
